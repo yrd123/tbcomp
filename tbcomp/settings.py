1969c1b2b159ca2b684de4940a3e8455b9b1764f
yrd <yash.deorah@somaiya.edu>
2020-07-27 18:15:46 +0530

    basic project  and static urls added

--- tbcomp/settings.py
+++ tbcomp/settings.py
@@ -118,4 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
--- tbcomp_app/urls.py
+++ tbcomp_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views,urls
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index,name="index"),
 ]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
