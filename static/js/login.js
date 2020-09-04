4382721f1abe8ed561c747661f59b277d1697ce6
Esha <esha.gupta@somaiya.edu>
2020-09-04 22:18:39 +0530

    Edited: Login Password Field

--- static/js/login.js
+++ static/js/login.js
@@ -1,8 +1,48 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var checkPassword = function(str)
+    {
+      var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+      return re.test(str);
+    };
+
+    var checkForm = function(e)
+    {
+      if(this.loginPassword.value != "") {
+        if(!checkPassword(this.loginPassword.value)) {
+          alert("The password you have entered is not valid!");
+          this.loginPassword.focus();
+          e.preventDefault();
+          return;
+        }
+      }
+    };
+
+    var myForm = document.getElementById("loginForm");
+    myForm.addEventListener("submit", checkForm, true);
+
+    var supports_input_validity = function()
+    {
+      var i = document.createElement("input");
+      return "setCustomValidity" in i;
+    }
+
+    if(supports_input_validity()) {
+
+      var pwd1Input = document.getElementById("loginPassword");
+      pwd1Input.setCustomValidity(pwd1Input.title);
+
+      pwd1Input.addEventListener("keyup", function(e) {
+        this.setCustomValidity(this.validity.patternMismatch ? pwd1Input.title : "");
+      }, false);
+    }
+  }, false);
+
 function loginValidation(){
   var email = document.forms["loginForm"]["emailId"].value;
   var email_patt = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  var password =  document.forms["loginForm"]["loginPassword"].value;
-  var passwordpattern=/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+  /*var password =  document.forms["loginForm"]["loginPassword"].value;
+  var passwordpattern=/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;*/
 
 	if(email_patt.test(email) == false )
 	{
@@ -13,7 +53,7 @@ function loginValidation(){
         document.getElementById('emailAlert').innerHTML = '';
 		document.getElementById('emailAlert').style.display = 'none';
     }
-
+    /*
     if (passwordpattern.test(password) == false)
     {
         document.getElementById('passwordAlert').innerHTML=" **Please enter valid Password";
@@ -22,7 +62,7 @@ function loginValidation(){
     else{
         document.getElementById('passwordAlert').innerHTML = '';
 		document.getElementById('passwordAlert').style.display = 'none';
-    }
+    }*/
 }
 
 function submitForm(){
